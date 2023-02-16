399766b38d158549c4eec081d4e2096e601313e3
notunderctrl <underctrl.io@gmail.com>
2023-02-16 20:55:00 +0300

    Find commands and subcommands

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const { getFolderPaths, getFilePaths } = require('./utils/getPaths');
+
 export class DiscordHandler {
   constructor({ client, commandsPath, eventsPath }) {
     if (!client) {
@@ -6,8 +8,47 @@ export class DiscordHandler {
 
     this._commandsPath = commandsPath;
     this._eventsPath = eventsPath;
-    this.init();
+    this._commands = [];
+    this._events = [];
+
+    this._commandsPath && this.getCommands();
+    this._eventsPath && this.getEvents();
   }
 
-  init() {}
+  getCommands() {
+    console.log('🔍 Finding commands...');
+    let categoryCount = 0;
+    let commandCount = 0;
+    let subcommandCount = 0;
+
+    const categoryPaths = getFolderPaths(this._commandsPath);
+
+    for (const categoryPath of categoryPaths) {
+      categoryCount++;
+
+      // Find any subcommand folders
+      const subcommandPaths = getFolderPaths(categoryPath);
+      if (subcommandPaths.length) {
+        for (const subcommandPath of subcommandPaths) {
+          const subcommandFilePaths = getFilePaths(subcommandPath);
+
+          for (const subcommandFilePath of subcommandFilePaths) {
+            subcommandCount++;
+          }
+        }
+      }
+
+      // Find regular command files
+      const commandPaths = getFilePaths(categoryPath);
+      for (const commandPath of commandPaths) {
+        commandCount++;
+      }
+    }
+
+    console.log(`${categoryCount} categories found.`);
+    console.log(`${commandCount} commands found.`);
+    console.log(`${subcommandCount} subcommands found.`);
+  }
+
+  getEvents() {}
 }
