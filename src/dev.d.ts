558083c16857f7c10d86f477eaf4a1930c43c668
notunderctrl <underctrl.io@gmail.com>
2023-03-21 00:06:29 +0300

    Remove unused typings

--- src/dev.d.ts
+++ src/dev.d.ts
@@ -1,16 +1,5 @@
 import { Client, APIApplicationCommand, SlashCommandBuilder } from 'discord.js';
 
-interface CommandHandler {
-  _client: Client;
-  _commandsPath?: string;
-  _eventsPath?: string;
-  _validationsPath?: string;
-  _testServer?: string;
-  _commands: Array<object>;
-  _commandsToRegister: Array<object>;
-  _validationFuncs: Array<function>;
-}
-
 interface LocalCommand extends APIApplicationCommand {
   deleted?: boolean;
   [key: string]: any;
