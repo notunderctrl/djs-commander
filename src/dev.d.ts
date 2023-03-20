ac610d3176e87401f7e5599f878cde2842fdef11
notunderctrl <underctrl.io@gmail.com>
2023-03-21 00:08:16 +0300

    Removed unused dev typings

--- src/dev.d.ts
+++ src/dev.d.ts
@@ -4,16 +4,3 @@ interface LocalCommand extends APIApplicationCommand {
   deleted?: boolean;
   [key: string]: any;
 }
-
-interface ApplicationCommandOption {
-  type: number;
-  name: string;
-  description: string;
-  required: boolean;
-  choices: Array<ApplicationCommandOptionChoice>;
-}
-
-interface ApplicationCommandOptionChoice {
-  name: string;
-  value: string | number;
-}
